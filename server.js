5e10d7d5454b6b9ca6196420099a894afff99fdd
amitpal603-ops <ap036776@gmail.com>
2025-10-16 00:01:04 +0530

    complete user login and sign up backend code

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 require('dotenv').config()
 const connectDB = require('./config/db')
+const userRouters = require('./routers/userRouters')
 const app  = express()
 connectDB()
 app.use(express.json())
-
+app.use('/user',userRouters)
 app.get('/', (req,res) => {
     res.send('Hello world !')
 })
